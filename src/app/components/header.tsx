4982a98ad6c6e8b3011b38f2fb9c3e3a7274e5e9
MariaB <lvlaria.15@gmail.com>
2023-11-15 19:27:52 +0300

    Первая попытка стайловые компоненты

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,18 +1,39 @@
+"use client";
 import Link from "next/link";
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: #bf4f74;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+`;
+
+const GreenButton = styled(Button)`
+  color: green;
+  border-color: black;
+`;
+
+const PurpleButton = styled(Button)`
+  color: purple;
+  border-color: black;
+`;
 
 export default function Header() {
   return (
     <div>
       <Link href="/">
         {" "}
-        <button>Logo</button>
+        <Button>Logo</Button>
       </Link>
 
       <Link href="/search">
-        <button>Search</button>
+        <GreenButton>Search</GreenButton>
       </Link>
       <Link href="/city">
-        <button>City</button>
+        <PurpleButton>City</PurpleButton>
       </Link>
     </div>
   );
